4d693ec5740cfe7b7884a890fcf6d3b80e37a5b0
IMAJEMAIL\ngajare_adm <ngajare@markem-imaje.com>
2022-12-27 19:56:55 +0530

    moved files from static website to net core

---
+++ csaElectric/csaElectric/EmailController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace csaElectric
+{
+    using System.Net;
+    using System.Net.Mail;
+    using Model;
+
+    [Route("api/[controller]")]
+    public class EmailController : Controller
+    {
+        // GET: api/<controller>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]EmailDetail emailDetail)
+        {
+            MailMessage message = new MailMessage();
+            SmtpClient smtp = new SmtpClient();
+            message.From = new MailAddress(emailDetail.SenderEmail);
+            message.To.Add(new MailAddress("nomeshg@gmail.com"));
+            message.Subject = "Customer Query";
+            // message.IsBodyHtml = true; //to make message body as html  
+
+            message.Body = emailDetail.GetEmailBody();
+            smtp.Port = 587;
+            smtp.Host = "smtp.gmail.com"; //for gmail host  
+            smtp.EnableSsl = true;
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new NetworkCredential("csaelectricbike@gmail.com", "qpbmnscvgowyqgau");
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtp.Send(message);
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ csaElectric/csaElectric/Model/EmailDetail.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace csaElectric.Model
+{
+    public class EmailDetail
+    {
+        public String Name { get; set; }
+
+        public String SenderEmail { get; set; }
+
+        public String  Subject { get; set; }
+
+        public String Message { get; set; }
+
+        public String GetEmailBody()
+        {
+            return $"Name: {Name}  \n\nSenderEmail: {SenderEmail} \n\nSubject: {Subject}\n\nMessage: {Message}";
+        }
+    }
+}
